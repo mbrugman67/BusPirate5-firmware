bbf9ca7dc73c9dd2c995fb415821a389cd41fb76
Matthew Brugman <mattb@linux.com>
2025-09-14 14:23:45 -0500

    added bus pirate specific IO handling for nuvoton

--- src/lib/nuvoton/buspirate.c
+++ src/lib/nuvoton/buspirate.c
@@ -4,6 +4,7 @@
  *
  * Copyright (c) 2021 Steve Markgraf <steve@steve-m.de>
  * Copyright (c) 2023-2024 Nikita Lita
+ * Copyright (c) 2025 Matt Brugman <mattb@linux.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -25,99 +26,116 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef ARDUINO
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "pico/stdlib.h"
 
+#include "pirate.h"
+#include "pirate/bio.h"
+#include "system_config.h"
+
+#define M_NUV_RESET BIO0
+#define M_NUV_CLOCK BIO1
+#define M_NUV_DATA BIO2
+#define M_NUV_TRIGGER BIO3
+
+static const char pin_labels[][5] = {"RST", "CLK", "DAT", "TRG"};
+
+/*
 static int8_t dat_dir = -1;
 static int8_t dat = -1;
 static int8_t rst = -1;
 static int8_t clk = -1;
 static uint8_t pgm_init_done = false;
-
+*/
 int N51PGM_init(void)
 {
+	// allocate the 3 I/O points
+    system_bio_update_purpose_and_label(true, M_NUV_RESET, BP_PIN_IO, pin_labels[0]);
+    system_bio_update_purpose_and_label(true, M_NUV_CLOCK, BP_PIN_IO, pin_labels[1]);
+    system_bio_update_purpose_and_label(true, M_NUV_DATA, BP_PIN_IO, pin_labels[2]);
+    system_bio_update_purpose_and_label(true, M_NUV_TRIGGER, BP_PIN_IO, pin_labels[3]);
+
+	// reset, clock, and trigger are outputs.  Set data to be an input to start with
+    bio_output(M_NUV_RESET);
+    bio_output(M_NUV_CLOCK);
+    bio_output(M_NUV_TRIGGER);
+    bio_input(M_NUV_DATA);
+
+    // set initial output states
+    bio_put(M_NUV_RESET, 0);
+    bio_put(M_NUV_CLOCK, 0);
+    bio_put(M_NUV_TRIGGER, 0);
+
 	return 0;
 }
 
 void N51PGM_set_dat(uint8_t val)
 {
-	if (dat_dir == 1) {
-		printf("%d", val);
-		dat = val;
-	} else {
-		printf("N51PGM_set_dat() called while dat_dir == 0\n");
-	}
-	
+	bio_put(M_NUV_DATA, (bool)val);
 }
 
 uint8_t N51PGM_get_dat(void)
 {
-	if (dat_dir == 0){
-		return dat;
-	} else {
-		printf("N51PGM_get_dat() called while dat_dir == 1\n");
-		return 0;
-	}
+	return (bio_get(M_NUV_DATA));
 }
 
 void N51PGM_set_rst(uint8_t val)
 {
-	rst = val;
+	bio_put(M_NUV_RESET, val);
 }
 
 void N51PGM_set_clk(uint8_t val)
 {
-	clk = val;
+	bio_put(M_NUV_CLOCK, val);
 }
 
 void N51PGM_dat_dir(uint8_t state)
 {
-	dat_dir = state;
+	if (state == true) {
+		bio_output(M_NUV_DATA);
+	} else {
+		bio_input(M_NUV_DATA);
+	}
+}
+
+void N51PGM_release_non_rst_pins(void)
+{
+    system_bio_update_purpose_and_label(false, M_NUV_RESET, BP_PIN_IO, pin_labels[0]);
+    system_bio_update_purpose_and_label(false, M_NUV_CLOCK, BP_PIN_IO, pin_labels[1]);
+    system_bio_update_purpose_and_label(false, M_NUV_DATA, BP_PIN_IO, pin_labels[2]);
+    system_bio_update_purpose_and_label(false, M_NUV_TRIGGER, BP_PIN_IO, pin_labels[3]);
+}
+
+void N51PGM_release_rst(void)
+{
+    system_bio_update_purpose_and_label(false, M_NUV_RESET, BP_PIN_IO, pin_labels[0]);
 }
 
 void N51PGM_deinit(uint8_t leave_reset_high)
 {
 	if (leave_reset_high)
-		N51PGM_set_rst(1);
+		N51PGM_set_rst(true);
 	else{
-		rst = -1;
+		N51PGM_release_rst();
 	}
-	clk = -1;
-	dat = -1;
-	dat_dir = -1;
-	pgm_init_done = false;
 
-}
-
-void N51PGM_release_pins(void)
-{
-	rst = -1;
-	clk = -1;
-	dat = -1;
-	dat_dir = -1;
-}
-
-void N51PGM_release_rst(void)
-{
-	rst = -1;
+	N51PGM_release_non_rst_pins();
 }
 
 void N51PGM_set_trigger(void)
 {
-	printf("N51PGM_set_trigger() called\n");
+	bio_put(M_NUV_TRIGGER, true);
 }
 
 uint32_t N51PGM_usleep(uint32_t usec)
 {
-	return usec;
+	busy_wait_us(usec);
+	return (usec);
 }
 
 void N51PGM_print(const char *msg)
 {
 	printf("%s", msg);
 }
-
-#endif
